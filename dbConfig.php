097e2cafa9b99ba8e9c9ae29eb0d8f6ae98b22f8
geraldfingburke <burkeg@goldmail.etsu.edu>
2021-10-10 13:27:37 -0400

    Add a guest dbConfig file

---
+++ dbConfig.php
@@ -0,0 +1,8 @@
+<?php
+
+define("DB_USER", "trigamed_guest");
+define("DB_PASS", "2rO7)~^^ijsx");
+define("DB_HOST", "atlshared8.ramnode.com");
+define("DB_NAME", "trigamed_simple_leaderboard");
+
+?>
